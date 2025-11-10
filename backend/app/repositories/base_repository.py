c1652dde827cd75d7bf82ce856079b0f24e630dd
Rurek5000 <kagorski99@gmail.com>
2025-11-10 10:50:46 +0100

    refactor: remove type ignore comment for psycopg2 import to enhance type safety

--- backend/app/repositories/base_repository.py
+++ backend/app/repositories/base_repository.py
@@ -4,7 +4,7 @@ import os
 from contextlib import contextmanager
 from typing import Any, Generator, Optional, Tuple
 
-import psycopg2  # type: ignore[import-untyped]
+import psycopg2
 from dotenv import load_dotenv
 
 from ..core.exceptions import DatabaseError
